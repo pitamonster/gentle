c1ef86a9da9784ecbc1c1a8bdcb5da821112d6e1
Arpita Dey <adey@aee.net>
2020-09-20 21:25:13 -0400

    Add gfortran to install deps

--- install_deps.sh
+++ install_deps.sh
@@ -7,7 +7,7 @@ echo "Installing dependencies..."
 # Install OS-specific dependencies
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	apt-get update -qq
-	apt-get install -y zlib1g-dev automake autoconf git \
+	apt-get install -y gfortran zlib1g-dev automake autoconf git \
 		libtool subversion libatlas3-base python3-pip \
 		python3-dev wget unzip python3
 	apt-get install -y ffmpeg || echo -n  "\n\nYou have to install ffmpeg from a PPA or from https://ffmpeg.org before you can run gentle\n\n"
